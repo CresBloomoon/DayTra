a0350f40497c72ebcc2b045634e8c59915a0c378
CresBloomoon <metroidm2421@gmail.com>
2023-11-04 16:16:34 +0900

    changed

--- 初級/Day14/elementary-javascript/js/script.js
+++ 初級/Day14/elementary-javascript/js/script.js
@@ -14,6 +14,24 @@
 // console.log(document.querySelector("h1").outerHTML);
 
 //属性で取得
-console.log(
-  document.querySelector('[href="https://www.daily-trial.com/"]').outerHTML
-);
+// console.log(
+//   document.querySelector('[href="https://www.daily-trial.com/"]').outerHTML
+// );
+
+// document.querySelector("#js-title").innerText = "Daily Trial";
+
+// const button = document.createElement("a");
+// button.innerText = "ログイン";
+// button.setAttribute("class", "btn");
+// button.setAttribute("href", "https://tokyofreelance.jp/");
+// document.querySelector("#js-btn-wrap").appendChild(button);
+
+// document.querySelector("#js-register").click();
+
+// window.onload = function () {
+//   alert("読み込みが完了しました");
+// };
+
+document.querySelector("#js-btn").addEventListener("click", function () {
+  alert("ボタンがクリックされました");
+});
