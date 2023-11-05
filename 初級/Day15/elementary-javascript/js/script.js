446e82906289d94c9dd00d080725d1b56ae628af
CresBloomoon <metroidm2421@gmail.com>
2023-11-05 15:09:40 +0900

    changed

--- 初級/Day15/elementary-javascript/js/script.js
+++ 初級/Day15/elementary-javascript/js/script.js
@@ -1,4 +1,11 @@
-jQuery();
+// jQuery();
 // document.querySelector("#js-title").innerText = "Daily Trial";
 
-jQuery("#js-title").text("Daily Trial");
+// jQuery("#js-title").text("Daily Trial");
+
+// jQuery("h1").css("color", "red");
+
+const elements = jQuery(".test");
+elements.each(function () {
+    console.log(jQuery(this).text())
+})
