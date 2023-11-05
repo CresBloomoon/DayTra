3b8e152a4646f125f8b51d9230469b313e91c7b9
CresBloomoon <metroidm2421@gmail.com>
2023-11-05 15:55:55 +0900

    changed

--- 初級/Day15/elementary-javascript/js/script.js
+++ 初級/Day15/elementary-javascript/js/script.js
@@ -5,7 +5,40 @@
 
 // jQuery("h1").css("color", "red");
 
-const elements = jQuery(".test");
-elements.each(function () {
-    console.log(jQuery(this).text())
-})
+// const elements = jQuery(".test");
+// elements.each(function () {
+//     console.log(jQuery(this).text())
+// })
+
+// jQuery("#js-section").find("p").css("color", "red");
+
+// //クラスの追加
+// jQuery("#js-btn").addClass("btn-register");
+
+// //クラスの削除
+// jQuery("#js-register").removeClass("btn-register");
+
+// //CSSプロパティの設定
+// jQuery("#js-title").css({
+//   color: "red",
+//   fontSize: "80px",
+// });
+
+// //文字列の変更
+// // jQuery("#js-title").text("Daily Trial");
+// const title = jQuery("#js-title").text();
+// alert(title);
+
+// jQuery("#js-title").css("color", "red").text("Daily Trial");
+
+//ふわっと表示
+// jQuery("#js-title").fadeIn(1500);
+
+//ふわっと消える
+// jQuery("#js-btn").fadeOut();
+
+//スライドして表示
+jQuery("#js-title").slideDown();
+
+//スライドして消える
+jQuery("#js-btn").slideUp();
