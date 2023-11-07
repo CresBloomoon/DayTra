41a23e4670245e713d41cc1cff3562fc2cd407de
CresBloomoon <metroidm2421@gmail.com>
2023-11-07 20:59:48 +0900

    changed

--- 初級/Day17/elementary-jquery/js/script.js
+++ 初級/Day17/elementary-jquery/js/script.js
@@ -5,7 +5,26 @@
 //     alert("おはよう");
 // })
 
-//jQuery
-jQuery("#js-button-alert").on("click", function(){
-    alert("おはよう");
+// //jQuery
+// jQuery("#js-button-alert").on("click", function(){
+//     alert("おはよう");
+// })
+
+
+// //アコーディオンメニューを作る
+// //jQuery
+// jQuery(".js-accordion").on("click", function() {
+//     jQuery(this).next().slideToggle();
+// })
+
+//トップへ戻る
+// jQuery("#js-button-top").on("click", function(){
+//     jQuery("html, body").animate({scrollTop: 0}, 500);
+// })
+
+//ドロワーメニュー
+jQuery("#js-button-drawer").on("click", function(){
+    jQuery(this).toggleClass("is-checked");
+    jQuery("#js-drawer").slideToggle();
+    jQuery("body").toggleClass("is-fixed");
 })
