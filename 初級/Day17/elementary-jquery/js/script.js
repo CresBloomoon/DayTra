48b45b67d539aad2c8969c9eaf6da378a5cf9487
CresBloomoon <metroidm2421@gmail.com>
2023-11-08 21:13:40 +0900

    changed

--- 初級/Day17/elementary-jquery/js/script.js
+++ 初級/Day17/elementary-jquery/js/script.js
@@ -28,3 +28,21 @@ jQuery("#js-button-drawer").on("click", function(){
     jQuery("#js-drawer").slideToggle();
     jQuery("body").toggleClass("is-fixed");
 })
+
+//フォームに入力された値をアラートに表示しよう
+jQuery("#js-form-name").on("submit", function(e){
+    e.preventDefault();
+
+    const nameInput = jQuery("#js-input-name");
+    const yourName = nameInput.val();
+
+    if (!yourName){
+        nameInput.next(".error-message").text("必須項目です");
+        return;
+    }
+
+    nameInput.next(".error-message").text("");
+    alert(`${yourName}さん、こんにちは`);
+
+    return false;
+})
